a985bc6586a54d5199f79c228140367cd60f4695
Maria <mariajescobar10@gmail.com>
2018-01-25 21:34:00 -0600

    added stuff

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,14 @@
 class ContactsController < ApplicationController
   def index
-    contacts = Contact.all 
+    @contacts = Contact.all
 
     search_term = params[:search]
-
     if search_term
-      contacts = contacts.where("first_name iLIKE ?", "%#{search_term}%")
-    end 
-
-    sort_attribute = params[:sort]
-
-    if sort_attribute
-      contacts = contacts.order(sort_attribute)
-    end 
-
-    render json: contacts.as_json
-  end 
+      @contacts = contacts.where("first_name iLIKE ? OR last_name iLIKE ?", "%#{search_term}%")
+    end
+    
+    render 'index.json.jbuilder'
+  end
 
   def create
     contact = Contact.new(
@@ -23,42 +16,41 @@ class ContactsController < ApplicationController
                           middle_name: params[:middle_name],
                           last_name: params[:last_name],
                           email: params[:email],
-                          phone_number: params[:phone_number],
-                          bio: params[:bio]
+                          bio: params[:bio],
+                          phone_number: params[:phone_number]
                           )
     if contact.save
-      render json: contact.as_json 
+      render json: contact.as_json
     else
       render json: {errors: contact.errors.full_messages}, status: :unprocessable_entity
-    end 
-  end 
+    end
+  end
 
   def show
-    contact = Contact.find(params[:id])
-    render json: contact.as_json
-  end 
+    @contact = Contact.find(params[:id])
+    render 'show.json.jbuilder'
+  end
 
   def update
-    contact = Contact.find(params[:id])
-
-    contact.first_name = params[:name] || contact.first_name
-    contact.middle_name = params[:middle_name] || contact.middle_name
-    contact.last_name = params[:last_name] || contact.last_name
-    contact.email = params[:email] || contact.email
-    contact.phone_number = params[:phone_number] || contact.phone_number 
-    contact.bio = params[:bio] || contact.bio 
+    @contact = Contact.find(params[:id])
     
-  if contact.save 
-    render json: contact.as_json
-  else 
-    render json: {errors: contact.errors.full_messages}, status: :unprocessable_entity
-  end 
-end 
+    @contact.first_name = params[:first_name] || contact.first_name
+    @contact.middle_name = params[:middle_name] || contact.middle_name
+    @contact.last_name = params[:last_name] || contact.last_name
+    @contact.email = params[:email] || contact.email
+    @contact.bio = params[:bio] || contact.bio
+    @contact.phone_number = params[:phone_number] || contact.phone_number
+    
+    if @contact.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: contact.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
 
   def destroy
     contact = Contact.find(params[:id])
     contact.destroy
-    render json: { message: "Successfully destroyed contact #{contact.id}"}
-  end 
-
-end 
+    render json: {message: "Successfully destroyed contact ##{contact.id}"}
+  end
+end
